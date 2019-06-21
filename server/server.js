8f559e77031bec5beafa325c7775c8e7d92d9cbc
tanisha2398 <tanishanegipro@gmail.com>
2019-06-21 17:30:43 +0530

    adding start in scripts taf

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const path = require("path");
 var express = require("express");
 var app = express();
-
+var port = process.env.PORT || 3009;
 const publicPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicPath));
 
-app.listen(3009, () => {
-  console.log("server started at port 3006");
+app.listen(port, () => {
+  console.log(`server started at port ${port}`);
 });
